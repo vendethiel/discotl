8e0d2ed583099cbd04617f701de12b9cf7c17d3a
vendethiel <vendethiel@hotmail.fr>
2017-04-02 01:28:26 +0200

    Add stream field

--- main.lisp
+++ main.lisp
@@ -8,15 +8,22 @@
 (ql:quickload :dexador :silent t)
 (ql:quickload :clss :silent t)
 
+(defconstant +sc2-css+ ".ev[style='background: url(/images/games/1.png) transparent no-repeat']" )
+(defvar *sc2-streams* (make-hash-table :test 'equal))
+(with-open-file (stream "streams")
+  (loop
+     for key = (read-line stream nil)
+     for value = (read-line stream nil)
+     until (or (null key) (null value))
+     do (setf (gethash key *sc2-streams*) value)))
+
 (defun parse-events (tl-doc)
-  (print
-   (loop
-      for event across (clss:select ".ev-feed .ev-block" tl-doc)
-      for classes = (plump:attribute event "class")
-      for name = (plump:text (aref (clss:select "span[data-event-id]" event) 0))
-      ; TODO filter by sc2
-      for timer = (plump:text (aref (clss:select ".ev-timer" event) 0))
-      for timer-text = (if (search "ev-live" classes) "LIVE" timer)
-      collect (list  name timer-text)))
-  )
+  (loop
+     for event across (clss:select ".ev-feed .ev-block" tl-doc)
+     for classes = (plump:attribute event "class")
+     for name = (plump:text (aref (clss:select "span[data-event-id]" event) 0))
+     for timer = (plump:text (aref (clss:select ".ev-timer" event) 0))
+     for timer-text = (if (search "ev-live" classes) "LIVE" timer)
+     when (plusp (length (clss:select +sc2-css+ event)))
+     collect (list name timer-text (gethash name *sc2-streams*))))
 (parse-events (plump:parse (dex:get "http://teamliquid.net/")))
